9f22882ade2c600c9439c6686dd762a72608e431
sloyardms <sloyard_ms@outlook.com>
2025-07-04 23:01:42 -0700

    empty media parser service module

---
+++ services/media-parser-service/src/test/java/com/sloyardms/mediaparserservice/MediaParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sloyardms.mediaparserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaParserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
